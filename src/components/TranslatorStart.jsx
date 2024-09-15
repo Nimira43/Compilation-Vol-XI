5747daa9148858144ad3da3a7b8c480237c4088b
Nimira43 <n.rabone72@aol.com>
2024-09-15 20:11:45 +0100

    Welcome message in English, French, Spanish and Chinese.

--- src/components/TranslatorStart.jsx
+++ src/components/TranslatorStart.jsx
@@ -2,10 +2,18 @@
 
 const TranslatorApp = () => {
   return (
-    <div>
-      
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div>
+        <span>Welcome. How can I help?</span>
+        <span>Bienvenue. Comment puis-je aider ?</span>
+        <span>Bienvenido. ¿Cómo puedo ayudar?</span>
+        <span>歡迎。我能幫忙嗎？</span>
+      </div>
+      <div>
+        <h1>Translator</h1>  
+      </div>
     </div>
-  )
+      )
 }
 
 export default TranslatorApp
