afd11049242c9899410b2544612baa00f0b7f3e6
Nimira43 <n.rabone72@aol.com>
2024-09-15 20:29:14 +0100

    Styled Welcome text.

--- src/components/TranslatorStart.jsx
+++ src/components/TranslatorStart.jsx
@@ -3,11 +3,11 @@
 const TranslatorApp = () => {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center p-12">
-      <div className="w-full h-64 bg-[#ff4500] rounded-t-full rounded-bl-full border-8 border-[#111]">
-        <span>Welcome</span>
-        <span>Bienvenue</span>
-        <span>Bienvenido</span>
-        <span>欢迎</span>
+      <div className="w-full h-64 bg-[#111] rounded-t-full rounded-bl-full border-8 border-[#ff4500] flex flex-col justify-center pr-6 text-[#87ceeb]">
+        <span className="font-fredericka text-6xl text-center">Welcome</span>
+        <span className="font-fredericka text-3xl text-center">Bienvenue</span>
+        <span className="font-fredericka text-4xl text-right">Bienvenido</span>
+        <span className="font-fredericka text-2xl text-right">欢迎</span>
       </div>
       <div>
         <h1>Translator</h1>  
