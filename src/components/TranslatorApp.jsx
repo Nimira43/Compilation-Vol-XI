2d98272d209a0b3a3c82c588f6f9ed20fbf46c55
Nimira43 <n.rabone72@aol.com>
2024-09-22 08:07:54 +0100

    Starting jsx markup for TranslatorApp.jsx

--- src/components/TranslatorApp.jsx
+++ src/components/TranslatorApp.jsx
@@ -1,7 +1,21 @@
 const TranslatorApp = () => {
   return (
     <div>
-      <button></button>
+      <button>
+        <i className="fa-solid fa-xmark"></i>
+      </button>
+      <div>
+        <div>English</div>
+        <i className="fa-solid fa-arrows-rotate"></i>
+        <div>English</div>
+      </div>
+      <div>
+        <textarea></textarea>
+        <div>0/200</div>
+        <button>
+          <i className="fa-solid fa-chevron-down"></i>
+        </button>
+      </div>
     </div>
   )
 }
